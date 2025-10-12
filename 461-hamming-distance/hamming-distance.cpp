7663b20ad2b9c4852e8debdeea96d16eb429d6e5
Sachin Sharma <149295563+SachinSharma3345@users.noreply.github.com>
2025-10-13 00:08:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (67.50%) - LeetSync

---
+++ 461-hamming-distance/hamming-distance.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int z = x^y;
+        int count = 0;
+        while(z){
+             count += (z & 1);
+            z = z>>1;
+        }
+        return count;
+
+        
+    }
+};
