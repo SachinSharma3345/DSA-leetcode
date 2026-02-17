b290e7833a62559bcb0b2502d6cd2aa63ad80ceb
Sachin Sharma <149295563+SachinSharma3345@users.noreply.github.com>
2026-02-17 13:32:32 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (20.99%) - LeetSync

---
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isSameAfterReversals(int n) {
+        if(n<9)return true;
+        // bool ans = true;
+        
+        if(n%10==0)return false;
+        
+        return true;
+
+        
+    }
+};
